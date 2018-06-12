223609545b020b7b24c33c84bc74bc1f1ed49e46
Louise Hauge Jessen <louisesmacbookpro@Louises-MacBook-Pro.local>
2018-06-12 22:15:51 +0200

    Fixed modal vindue with the right information for clicked element

--- build/script.js
+++ build/script.js
@@ -4,12 +4,13 @@
 let template_bartender = document.querySelector("#bartender_template").content;
 let template_beerType = document.querySelector("#beertypes_template").content;
 let template_orders = document.querySelector("#order_template").content;
-// let template_que = document.querySelector("#que_template").content;
 let template_storage = document.querySelector("#storage_template").content;
 let data;
 
 // WHEN THE SITE LOADS
 document.addEventListener("DOMContentLoaded",loadScript);
+
+const queHistory = []; // [30,30,45,55,25,5,15];
 
 function loadScript(){
 
@@ -19,8 +20,17 @@ function loadScript(){
     document.querySelector("#bartender_reciever").innerHTML = '';
     document.querySelector("#beerInfo_reciever").innerHTML = '';
     document.querySelector("#customerOrder_reciever").innerHTML = '';
-    // document.querySelector("#que_reciever").innerHTML = '';
     document.querySelector("#storage_reciever").innerHTML = '';
+
+    // console.log("Queue: ", data.queue);
+    queHistory.push( data.queue.length );
+
+    if( queHistory.length > 40 ) // hvor mange punkter der må være i grafen, før den scroller
+    {
+        // console.log("scroll queueueueu");
+        queHistory.shift();
+    }
+    drawQueueHistory();
 
                 // BARTENDERS
                 data.bartenders.forEach(function(e){
@@ -71,6 +81,37 @@ function loadScript(){
                             let beerLabel = e.label;
                             clone_beerType.querySelector(".beer_img img").src = "img/new_type/"+beerLabel;
 
+                            // Click event for details
+                            clone_beerType.querySelector(".beer_details").addEventListener("click",function(){
+                                console.log(e.name);
+
+                                document.querySelector(".beer_info_name").textContent = e.name;
+                                document.querySelector(".top_information_img img").src = "img/new_type/"+beerLabel;
+                                document.querySelector(".categori").textContent = e.category;
+                                document.querySelector(".alkoholprocent").textContent = e.alc+"%";
+                                document.querySelector(".populær").textContent = e.popularity;
+                                document.querySelector(".speed").textContent = e.pouringSpeed;
+
+                                document.querySelector(".appearence").textContent = e.description.appearance;
+                                document.querySelector(".aroma").textContent = e.description.aroma;
+                                // document.querySelector(".flavor").textContent = e.description.flavor;
+                                document.querySelector(".mouthfeel").textContent = e.description.mouthfeel;
+                                
+                                // Showing modal when the btn is clicked
+                                document.querySelector(".modal_window").style.visibility = "visible";
+
+                                document.querySelector(".luk").addEventListener("click",function(){
+                                    console.log("close modal")
+
+                                    document.querySelector(".modal_window").style.visibility = "hidden";
+
+                                })
+                                
+
+                            });
+
+
+
                             // APPENDING IT TO THE HTML
                             document.querySelector("#beerInfo_reciever").appendChild(clone_beerType);
                 });
@@ -78,8 +119,6 @@ function loadScript(){
 
                 // SERVING
                 data.serving.forEach(function(e){
-                    
-                    
                     
                             // CLONE TEMPLATE FOR ORDERS
                             let clone_orders = template_orders.cloneNode(true);
@@ -90,7 +129,7 @@ function loadScript(){
                                 let beertype = data.beertypes.find(function(el){
                                     return el.name === anOrder
                                 });
-                                console.log(beertype)
+                                // console.log(beertype)
                                 let img = document.createElement('img');
                                 img.src= "img/new_type/"+beertype.label;
                                 imgHolder.appendChild(img)
@@ -102,27 +141,6 @@ function loadScript(){
 
                 });
 
-
-                // QUE
-                let peopleInQue = data.queue.length;
-                let point = "14,52"; // Where the graphs starting from
-
-                 // Tilføjer nye punkter til vores polyline
-                 point += ' '+15+','+(52-peopleInQue);
-
-
-                 // En del med push 
-                let voresarraymedvaerdier = {point};
-
-
-
-                console.log(voresarraymedvaerdier);
-                // let arr = data.queue;
-
-
-                // ForEACH array, 
-        
-                document.querySelector("#que_reciever polyline").setAttribute("points", point);
 
                 // STORAGE
                 data.storage.forEach(function(e){
@@ -138,7 +156,27 @@ function loadScript(){
                                 clone_storage.querySelector(".storage_wrapper").style.background = "linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)";
                             }
 
+                            // ADDING LABEL TO THE STORAGE
+                            let beer_name = e.name.toLowerCase();
+                            clone_storage.querySelector(".storage_box img").src = "img/storage/"+beer_name+".svg";
+
+                            // ORDER NOW WINDOWS
+                            clone_storage.querySelector(".storage_now").addEventListener("click", function(ex){
+
+                                // console.log("HEJ")
+                                document.querySelector(".modal_order_kegs").style.display = "block";
+
+                                document.querySelector(".close").addEventListener("click", function(){
+
+                                    document.querySelector(".modal_order_kegs").style.display = "none";
+                                })
+                                
+                            });
+                            
+
                             document.querySelector("#storage_reciever").appendChild(clone_storage);
+
+
 
                 });
 
@@ -148,48 +186,69 @@ function loadScript(){
                 let goalDate = new Date(0, 0, 0, 21, 59, 0, 0);
 
                 document.querySelector(".time_stamp").textContent = (goalDate.getHours() - today.getHours()+" hours & ");
-                document.querySelector(".minuts").textContent = (goalDate.getMinutes() - today.getMinutes()+" minuts");
+                document.querySelector(".minuts").textContent = (goalDate.getMinutes() - today.getMinutes()+" minuts");            
+                
+}
 
-                // IF STATEMENT THAT CHANGE THE HTML TEXT TO "WE ARE CLOSED" - WHEN TIME IS AFTER 22:00
+
+// DRAWRING QUE
+function drawQueueHistory() {
+                // byg points efter queueHistory
+                //<polyline points="15,30 20,30 25,45 30,55 35,25 40,5 45,15" fill="none" stroke="white" />
+                let points = "";
+                let xStart = 15;
+                const xIncrement = 5;
+
+                let x = xStart;
+
+                // data.que på baggrund af vores q
+                queHistory.forEach( function(q) {
+
+                    // define pointerne til formel 
+                    const top = 6.7;
+                    const bot = 52;
+
+                    // Formel der udregner pointsne
+                    const y = (1-q/25)*(bot-top)+top;  // Formel udregnet af Peter Lind
+                    points+= x+","+y+" ";
+
+                    // hver gang vi har et point gør læg 5 til 
+                    x+=xIncrement;
+                });
+
+                document.querySelector("#que_reciever polyline").setAttribute("points", points);
 
 
-
+}
 
                 // MESSAGES
-                // Remove element on click, via chechbox - så man ikke længere kan se elementet (Brug animationsend & display: none, så de andre elementer rykker sig op)
+                // Remove element on click, 
                 document.querySelector("#delete").addEventListener("click",function(ex){
+                        console.log("hej")
 
                     let bla = ex.target.parentElement;
-                
                     bla.classList.add("slideOut");
                     
                     bla.addEventListener("animationend", function(){
                         bla.style.display = "none";
                     });
-                    
                 });
-                
-                
-}
+
+                // BURGER MENU 
+                document.querySelector(".burger-menu").addEventListener("click", function(){
+
+                    let menu = document.querySelector(".burger-menu");
+                    let menu_text = document.querySelector("#menu_info");
+                    
+                        menu.classList.toggle("burger-menu--opened");
+                        menu.classList.toggle("burger-menu--closed");
+                    
+                        menu_text.classList.toggle("hide");
+                    
+                    });
 
 
-
-
-
-document.querySelector(".burger-menu").addEventListener("click", function(){
-
-    let menu = document.querySelector(".burger-menu");
-    let menu_text = document.querySelector("#menu_info");
-    
-        menu.classList.toggle("burger-menu--opened");
-        menu.classList.toggle("burger-menu--closed");
-    
-        menu_text.classList.toggle("hide");
-    
-    });
-
-
-    // setInterval(function(){ 
-    //     loadScript();    
-    // }, 10000);
+     setInterval(function(){ 
+         loadScript();    
+     }, 10000);
 
